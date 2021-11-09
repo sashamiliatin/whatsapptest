77b2feb22f08e229cc5f06213930b8d00b7dde87
Sasha <sashamiliatin@gmail.com>
2021-11-09 12:08:17 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='whatsapptest',
+    description='Aiola testing infrastructure',
+    version='0.0.1',
+    url='https://github.com/sashamiliatin/whatsapptest',
+    keywords=['aiola'],
+    install_requires=['pytest']
+)
